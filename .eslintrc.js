a12882f4f963b0afe50b132d2650d0f59f542a2e
Jamar Powell <jamarpowell.dev@gmail.com>
2020-04-29 10:52:58 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "rules": {
+    "space-before-function-paren": ["error", "always"]
+  },
+  "env" : {
+    "browser": true,
+    "node": true
+  },
+  "globals": {
+    "document": false
+  }
+}
